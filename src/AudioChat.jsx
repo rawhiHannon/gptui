bbbc1f8d96d873d6e0ab7f2668cbb22b2862034b
prawhi <rawhi.h@pandats.com>
2023-11-15 17:42:30 +0200

    .

--- src/AudioChat.jsx
+++ src/AudioChat.jsx
@@ -3,6 +3,7 @@ import Manager from "./manager";
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import Avatar from '@mui/material/Avatar';
+import { faL } from "@fortawesome/free-solid-svg-icons";
 
 const AudioChat = () => {
   const [text, setText] = useState('');
@@ -22,6 +23,15 @@ const AudioChat = () => {
     chatMessagesRef.current.scrollTop = chatMessagesRef.current.scrollHeight;
   }, [messages]);
 
+  let isPlaying = false;
+  let finished = true;
+
+  useEffect(() => {
+    if (!isPlaying) {
+      setIsGptSpeaking(false);
+    }
+  }, [isPlaying]);
+
   const processAudioQueue = () => {
     if (audioQueue.current.length > 0 && !isProcessingAudio.current) {
       isProcessingAudio.current = true;
@@ -36,78 +46,80 @@ const AudioChat = () => {
     }
   };
   
-  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-  let sourceNode = null;
-  let audioBuffer = null;
+  let streamFinished = true;
+
+  function playNextAudioChunk() {
+    if(currentMessageAudioChunks.current.length == 0) {
+      streamFinished = true;
+    }
+    if (isPlaying || currentMessageAudioChunks.current.length === 0) {
+      // Already playing or no chunks to play
+      return;
+    }
+  
+    isPlaying = true;
+    streamFinished = false;
+    setIsGptSpeaking(true);
+    const audioBlob = currentMessageAudioChunks.current.shift();
+    
+    if(audioBlob == null) {
+      setIsGptSpeaking(false);
+      finished = true;
+    }
+
+    const reader = new FileReader();
+    reader.onload = function() {
+      const arrayBuffer = this.result;
+      audioContext.decodeAudioData(arrayBuffer, playBuffer, errorHandler);
+    };
+    reader.readAsArrayBuffer(audioBlob);
+  }
+  
+  function playBuffer(audioBuffer) {
+    const source = audioContext.createBufferSource();
+    source.buffer = audioBuffer;
+    source.connect(audioContext.destination);
+    source.start();
+  
+    source.onended = () => {
+      isPlaying = false;
+      this.forceUpdate(); // If you're using class components
+      playNextAudioChunk(); // Play the next chunk if available
+    };
+  }
+  
+  function errorHandler(e) {
+    console.error("Error decoding audio data: " + e.err);
+  }
+
+
+  // const audioContext = new (window.AudioContext || window.webkitAudioContext)();
   let isAudioStreaming = false;  
 
-  const playAudioChunk = async (arrayBuffer) => {
-    try {
-      const additionalBuffer = await audioContext.decodeAudioData(arrayBuffer);
-  
-      if (!audioBuffer) {
-        audioBuffer = additionalBuffer;
-      } else {
-        const tmpBuffer = audioContext.createBuffer(
-          audioBuffer.numberOfChannels,
-          audioBuffer.length + additionalBuffer.length,
-          audioBuffer.sampleRate
-        );
-  
-        for (let channel = 0; channel < audioBuffer.numberOfChannels; channel++) {
-          const channelData = tmpBuffer.getChannelData(channel);
-          channelData.set(audioBuffer.getChannelData(channel), 0);
-          channelData.set(additionalBuffer.getChannelData(channel), audioBuffer.length);
-        }
-  
-        audioBuffer = tmpBuffer;
-      }
-  
-      if (sourceNode) {
-        currentPlaybackTime = audioContext.currentTime - sourceNode.startTime;
-        sourceNode.disconnect();
-      }
-  
-      sourceNode = audioContext.createBufferSource();
-      sourceNode.buffer = audioBuffer;
-      sourceNode.connect(audioContext.destination);
-      sourceNode.start(0, currentPlaybackTime);
-      sourceNode.startTime = audioContext.currentTime - currentPlaybackTime;
-    } catch (error) {
-      console.error('Error playing audio chunk:', error);
-    }
-  };
-  
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
   const receiveChatMessage = (message) => {
     if (message.text === "StreamComplete") {
-      setIsGptSpeaking(false);
       isAudioStreaming = false;
       audioQueue.current.push([...currentMessageAudioChunks.current]);
-      currentMessageAudioChunks.current = [];
+      // currentMessageAudioChunks.current = [];
       if (!isProcessingAudio.current) {
         processAudioQueue();
       }
-      audioBuffer = null
-      sourceNode = null
-
+      currentMessageAudioChunks.current.push(null);
     } else {
       // This block handles the incoming audio chunks
       if (!isAudioStreaming) {
-        setIsGptSpeaking(true);
         isAudioStreaming = true;
-        currentMessageAudioChunks.current = [];
+        // currentMessageAudioChunks.current = [];
       }
-  
+
       const audioBlob = base64ToBlob(message.text, 'audio/mpeg');
       currentMessageAudioChunks.current.push(audioBlob);
   
-      // Convert the Blob to ArrayBuffer for the audio context
-      const reader = new FileReader();
-      reader.onload = function() {
-        const arrayBuffer = this.result;
-        playAudioChunk(arrayBuffer);
-      };
-      reader.readAsArrayBuffer(audioBlob);
+      if (streamFinished == true) {
+        playNextAudioChunk();
+      }
     }
   };
   
@@ -120,6 +132,7 @@ const AudioChat = () => {
     const byteArray = new Uint8Array(byteNumbers);
     return new Blob([byteArray], { type: contentType });
   };
+
 
   const sendMessage = () => {
     let msg = { 
--- src/websockets-manager.js
+++ src/websockets-manager.js
@@ -1,139 +1,162 @@
 function WebSocketsManager() {
-    this.ws = null;
-    this.serverUrl = "ws://localhost:7879/api/ws";
-    this.roomInput = null;
-    this.rooms = [];
-    this.manager = null;
-    this.user = {
-      name: "rawhi"
-    };
-    this.token = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijk3OWVkMTU1OTdhYjM1Zjc4MjljZTc0NDMwN2I3OTNiN2ViZWIyZjAiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vYmlibGVwaWNzLTU2ZmM1IiwiYXVkIjoiYmlibGVwaWNzLTU2ZmM1IiwiYXV0aF90aW1lIjoxNjc5NjEwOTM1LCJ1c2VyX2lkIjoiVWdjdDJyZXVkMVRDZUdYRlV3MG0zcndnQVdBMiIsInN1YiI6IlVnY3QycmV1ZDFUQ2VHWEZVdzBtM3J3Z0FXQTIiLCJpYXQiOjE2Nzk2MTA5MzUsImV4cCI6MTY3OTYxNDUzNSwiZW1haWwiOiJycmF3aGlAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbInJyYXdoaUBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.AezYbeCG4El1OExH8edWLrn1L2an8rx-zec9Ai1ANpCCThbuHjn4qUUSIGwMZ_Oz8enXlwIJeO995vUzBO5DhLWaOQMutsS2CE0fu62WvnVa9sgi8m5M85utsJEBBs03AgpGb-BGoXgL06FoNYbn4rDv1J6OFrxz63OfL5joX9rkzXD9kxHKwrC_xmErjqLI_-UFNyo7Z267nnIgj94KvNm9hzs7ijHjzWyB7vQ3rPHbAQj-jv0Kcs1cu3c9FGh5K_C3Rl-rH4yKDVro7jRqSsSDiqTl1rxgArBeCSkqS1241GeTgnerbY8i8BE09styP_ydbMeOflArMnkaduit2w"
-    this.users = [];
-    this.ready = this.connectToWebsocket()
+  this.ws = null;
+  this.serverUrl = "ws://localhost:7879/api/ws";
+  this.roomInput = null;
+  this.rooms = [];
+  this.manager = null;
+  this.handler = null;
+  this.user = {
+    name: "rawhi"
+  };
+  this.users = [];
+  this.reconnectInterval = 1000;
+  this.maxReconnectInterval = 30000;
+  this.reconnectAttempts = 0;
+  this.ready = this.connectToWebsocket();
 }
 var self = WebSocketsManager.prototype;
 
 self.setManager = function(manager) {
-  this.manager = manager
+this.manager = manager
 }
 
 //TODO: handle reject
 self.connectToWebsocket = function() {
-  return new Promise((resolve, reject) => {
-    this.ws = new WebSocket(this.serverUrl + "?bearer=1234dd56");
-    this.ws.addEventListener('open', (event) => { this.onWebsocketOpen(event, resolve) });
-    this.ws.addEventListener('message', (event) => { this.handleNewMessage(event) });
-    this.ws.addEventListener('close', (event) => { this.handleClose(event) });
-  })
+return new Promise((resolve, reject) => {
+  this.ws = new WebSocket(this.serverUrl + "?bearer=1233456&host=agentbuddy.me");
+  this.ws.addEventListener('open', (event) => { this.onWebsocketOpen(event, resolve) });
+  this.ws.addEventListener('message', (event) => { this.handleNewMessage(event) });
+  this.ws.addEventListener('close', (event) => { this.handleClose(event) });
+})
 }
 
 self.handleClose = function(event) {
-    alert("Server closed the connection, please refresh the page")
+console.log("Server closed the connection, attempting to reconnect...");
+this.reconnectAttempts = 0; // Reset the number of reconnection attempts
+setTimeout(() => {
+    this.reconnect();
+}, this.reconnectInterval);
+}
+
+self.reconnect = async function() {
+try {
+    this.ready = this.connectToWebsocket();
+    await this.ready;
+    console.log("Reconnected to the server");
+    this.reconnectAttempts = 0; // Reset the number of reconnection attempts
+} catch (err) {
+    console.error("Failed to reconnect, retrying in", this.reconnectInterval, "ms");
+    this.reconnectAttempts++;
+    const nextInterval = Math.min(this.reconnectInterval * Math.pow(2, this.reconnectAttempts), this.maxReconnectInterval);
+    setTimeout(() => {
+        this.reconnect();
+    }, nextInterval);
+}
 }
 
 self.onWebsocketOpen = function(event, resolve) {
-    if(resolve) {
-      try {
-        resolve();
-      } catch(err) {}
-    }
-    console.log("connected to WS!");
+  if(resolve) {
+    try {
+      resolve();
+    } catch(err) {}
+  }
+  console.log("connected to WS!");
 }
 
 self.handleNewMessage = function(event) {
-    let data = event.data;
-    data = data.split(/\r?\n/);
+  let data = event.data;
+  data = data.split(/\r?\n/);
 
-    for (let i = 0; i < data.length; i++) {
-      let msg = JSON.parse(data[i]);
-      switch (msg.action) {
-        case "stream":
-          this.handleStreamMessage(msg);
-          break;
-        case "event":
-          this.handleEventMessage(msg);
-          break;
-        case "chat":
-          this.handleEventMessage(msg);
-          break;
-        default:
-          break;
-      }
-
+  for (let i = 0; i < data.length; i++) {
+    let msg = JSON.parse(data[i]);
+    switch (msg.action) {
+      case "stream":
+        this.handleStreamMessage(msg);
+        break;
+      case "event":
+        this.handleEventMessage(msg);
+        break;
+      case "chat":
+        this.handleEventMessage(msg);
+        break;
+      default:
+        break;
     }
+
+  }
 }
 
 self.handleStreamMessage = function(msg) {
-  this.manager.handleSocketMessage(msg.action, msg.target, msg.message);
+this.manager.handleSocketMessage(msg.action, msg.target, msg.message);
 }
 
 self.handleEventMessage = function(msg) {
-    this.manager.handleSocketMessage(msg.action, msg.target, msg.message);
-}
-
-self.handleChatMessage = function(msg) {
   this.manager.handleSocketMessage(msg.action, msg.target, msg.message);
 }
 
+self.handleChatMessage = function(msg) {
+this.manager.handleSocketMessage(msg.action, msg.target, msg.message);
+}
+
 self.sendMessageToRoom = function(room) {
-    if (room.newMessage !== "") {
-      this.ws.send(JSON.stringify({
-        action: 'message',
-        message: room.newMessage,
-        target: {
-          id: room.id,
-          name: room.name
-        }
-      }));
-      room.newMessage = "";
-    }
+  if (room.newMessage !== "") {
+    this.ws.send(JSON.stringify({
+      action: 'message',
+      message: room.newMessage,
+      target: {
+        id: room.id,
+        name: room.name
+      }
+    }));
+    room.newMessage = "";
+  }
 }
 
 self.sendChatMessage = async function(msg) {
-  await this.ready;
-    this.ws.send(JSON.stringify({
-      action: 'chat',
-      message: msg
-    }));
-}
-
-self.sendSettingsMessage = async function(data) {
-  await this.ready;
+await this.ready;
   this.ws.send(JSON.stringify({
-    action: 'settings',
-    data: {figure_id: 1}
+    action: 'chat',
+    message: msg
   }));
 }
 
+self.sendSettingsMessage = async function(data) {
+await this.ready;
+this.ws.send(JSON.stringify({
+  action: 'settings',
+  data: {figure_id: 1}
+}));
+}
+
 self.findRoom = function(roomId) {
-    for (let i = 0; i < this.rooms.length; i++) {
-      if (this.rooms[i].id === roomId) {
-        return this.rooms[i];
-      }
+  for (let i = 0; i < this.rooms.length; i++) {
+    if (this.rooms[i].id === roomId) {
+      return this.rooms[i];
     }
+  }
 }
 
 self.joinRoom = async function(room, data) {
-    await this.ready;
-    console.log("joining rawhi room")
-    this.ws.send(JSON.stringify({
-      action: 'join-room',
-      message: room,
-      data: data || ""
-    }));
-    this.roomInput = "";
+  await this.ready;
+  console.log("joining rawhi room")
+  this.ws.send(JSON.stringify({
+    action: 'join-room',
+    message: room,
+    data: data || ""
+  }));
+  this.roomInput = "";
 }
 
 self.leaveRoom = async function(room) {
-	await this.ready;
-    this.ws.send(JSON.stringify({ action: 'leave-room', message: room }));
+await this.ready;
+  this.ws.send(JSON.stringify({ action: 'leave-room', message: room }));
 
-    for (let i = 0; i < this.rooms.length; i++) {
-      if (this.rooms[i].id === room.id) {
-        this.rooms.splice(i, 1);
-        break;
-      }
+  for (let i = 0; i < this.rooms.length; i++) {
+    if (this.rooms[i].id === room.id) {
+      this.rooms.splice(i, 1);
+      break;
     }
+  }
 }
 
 var nm = new WebSocketsManager();
