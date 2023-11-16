b3927ccb915ead2a4c9fbe94b1f045e7d7611ea5
rawhi <rrawhi@gmail.com>
2023-11-17 01:20:57 +0200

    .

--- src/AudioChat.jsx
+++ src/AudioChat.jsx
@@ -4,6 +4,10 @@ import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import Avatar from '@mui/material/Avatar';
 import { faL } from "@fortawesome/free-solid-svg-icons";
+import MicIcon from '@mui/icons-material/Mic';
+import MicOffIcon from '@mui/icons-material/Stop';
+import useSound from 'use-sound';
+import boopSfx from './interface-124464.mp3';
 
 const AudioChat = () => {
   const [text, setText] = useState('');
@@ -11,9 +15,33 @@ const AudioChat = () => {
   const audioQueue = useRef([]);
   const chatMessagesRef = useRef(null);
   const currentMessageAudioChunks = useRef([]);
+  const historyMessageAudioChunks = useRef([]);
   const isProcessingAudio = useRef(false);
   const [isGptSpeaking, setIsGptSpeaking] = useState(false);
-  let currentPlaybackTime = 0;
+  const [play] = useSound(boopSfx);
+
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [hasSpeechEnded, setHasSpeechEnded] = useState(false);
+
+  useEffect(() => {
+    // Add global mouseup event listener
+    const handleGlobalMouseUp = () => {
+      if (isMouseDown) {
+        setIsRecording(false); // Stop recording when mouse is released
+        setIsMouseDown(false);
+        if (hasSpeechEnded && transcriptAccumulator.current.trim().length > 0) {
+          sendTextMessage(transcriptAccumulator.current);
+          transcriptAccumulator.current = "";
+        }
+      }
+    };
+
+    window.addEventListener('mouseup', handleGlobalMouseUp);
+
+    return () => {
+      window.removeEventListener('mouseup', handleGlobalMouseUp);
+    };
+  }, [isMouseDown]);
 
   useEffect(() => {
     Manager.registerChatHandler(receiveChatMessage);
@@ -24,13 +52,61 @@ const AudioChat = () => {
   }, [messages]);
 
   let isPlaying = false;
-  let finished = true;
 
   useEffect(() => {
     if (!isPlaying) {
       setIsGptSpeaking(false);
     }
   }, [isPlaying]);
+
+
+  const [isRecording, setIsRecording] = useState(false);
+  const speechRecognitionRef = useRef(null);
+  const audioStartRef = useRef(new Audio('interface-124464.mp3'));
+  const transcriptAccumulator = useRef("");  // Local variable for accumulating transcript
+
+  // Initialize Speech Recognition
+  useEffect(() => {
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    if (SpeechRecognition) {
+      speechRecognitionRef.current = new SpeechRecognition();
+      speechRecognitionRef.current.continuous = false;
+      speechRecognitionRef.current.lang = 'en-US';
+      speechRecognitionRef.current.interimResults = false;
+
+      speechRecognitionRef.current.onresult = (event) => {
+        const transcript = event.results[0][0].transcript;
+        transcriptAccumulator.current += transcript; // Accumulate transcript
+      };
+
+      speechRecognitionRef.current.onend = () => {
+        setHasSpeechEnded(true);
+        if (!isMouseDown && transcriptAccumulator.current.trim().length > 0) {
+          sendTextMessage(transcriptAccumulator.current);
+          transcriptAccumulator.current = "";
+        }
+      };
+
+      speechRecognitionRef.current.onerror = (event) => {
+        console.error('Speech recognition error', event.error);
+      };
+    } else {
+      console.error('Speech recognition not supported in this browser.');
+    }
+  }, []);
+
+  const startRecording = () => {
+    setIsRecording(true);
+    transcriptAccumulator.current = "";  // Clear accumulator
+    setIsMouseDown(true);
+    speechRecognitionRef.current.start();
+  };
+
+  const stopRecording = () => {
+    setIsRecording(false);
+    speechRecognitionRef.current.stop();
+    // Do not send the message here anymore
+  };
 
   const processAudioQueue = () => {
     if (audioQueue.current.length > 0 && !isProcessingAudio.current) {
@@ -46,34 +122,39 @@ const AudioChat = () => {
     }
   };
   
-  let streamFinished = true;
-
   function playNextAudioChunk() {
-    if(currentMessageAudioChunks.current.length == 0) {
-      streamFinished = true;
-    }
     if (isPlaying || currentMessageAudioChunks.current.length === 0) {
       // Already playing or no chunks to play
       return;
     }
   
-    isPlaying = true;
-    streamFinished = false;
     setIsGptSpeaking(true);
-    const audioBlob = currentMessageAudioChunks.current.shift();
+    isPlaying = true;
+  
+    // Determine if the last item is "StreamComplete"
+    const hasStreamComplete = currentMessageAudioChunks.current[currentMessageAudioChunks.current.length - 1] === "StreamComplete";
+  
+    // Extract all chunks except "StreamComplete" if it's present
+    const audioBlobs = hasStreamComplete ? currentMessageAudioChunks.current.slice(0, -1) : [...currentMessageAudioChunks.current];
     
-    if(audioBlob == null) {
+    // Clear the array except for "StreamComplete" if it's present
+    currentMessageAudioChunks.current = hasStreamComplete ? ["StreamComplete"] : [];
+  
+    if (audioBlobs.length === 0) {
       setIsGptSpeaking(false);
-      finished = true;
+      isPlaying = false;
+      return;
     }
-
+  
+    const combinedBlob = new Blob(audioBlobs, { type: 'audio/mpeg' });
     const reader = new FileReader();
     reader.onload = function() {
       const arrayBuffer = this.result;
       audioContext.decodeAudioData(arrayBuffer, playBuffer, errorHandler);
     };
-    reader.readAsArrayBuffer(audioBlob);
+    reader.readAsArrayBuffer(combinedBlob);
   }
+  
   
   function playBuffer(audioBuffer) {
     const source = audioContext.createBufferSource();
@@ -83,13 +164,15 @@ const AudioChat = () => {
   
     source.onended = () => {
       isPlaying = false;
-      this.forceUpdate(); // If you're using class components
+      setIsGptSpeaking(false);
       playNextAudioChunk(); // Play the next chunk if available
     };
   }
   
   function errorHandler(e) {
     console.error("Error decoding audio data: " + e.err);
+    isPlaying = false;
+    setIsGptSpeaking(false);
   }
 
 
@@ -98,26 +181,35 @@ const AudioChat = () => {
 
   const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
+  async function sendAudioToSTTService(audioBlob) {
+    // Implement the actual call to the STT service here
+    // Return the transcribed text
+    return "Transcribed text from audio";
+  }
+
   const receiveChatMessage = (message) => {
     if (message.text === "StreamComplete") {
       isAudioStreaming = false;
-      audioQueue.current.push([...currentMessageAudioChunks.current]);
-      // currentMessageAudioChunks.current = [];
+      audioQueue.current.push([...historyMessageAudioChunks.current]);
+      historyMessageAudioChunks.current = [];
+      historyMessageAudioChunks.current.push(message.text);
       if (!isProcessingAudio.current) {
-        processAudioQueue();
+        // processAudioQueue();
       }
-      currentMessageAudioChunks.current.push(null);
+      // const transcribedText = "Recevied all chunks.";
+      // setMessages(messages => [...messages, { id: messages.length + 1, text: transcribedText, user: { name: "GPT", avatar: "gpt", color: "maroon" } }]);
     } else {
       // This block handles the incoming audio chunks
       if (!isAudioStreaming) {
         isAudioStreaming = true;
-        // currentMessageAudioChunks.current = [];
+        historyMessageAudioChunks.current = [];
       }
 
       const audioBlob = base64ToBlob(message.text, 'audio/mpeg');
       currentMessageAudioChunks.current.push(audioBlob);
-  
-      if (streamFinished == true) {
+      historyMessageAudioChunks.current.push(audioBlob);
+
+      if (isPlaying == false) {
         playNextAudioChunk();
       }
     }
@@ -145,30 +237,79 @@ const AudioChat = () => {
     setText("");
   };
 
+  const sendTextMessage = (textData) => {
+    let newMessage = { 
+      id: messages.length + 1, 
+      text: textData, 
+      user: { name: "rawhi", avatar: "r", color: "green" } 
+    };
+    Manager.send(newMessage.text);
+    setMessages(messages => [...messages, newMessage]); // Update this line
+    setText("");
+  };
+
   const handleKeyDown = (e) => {
     if (e.key === 'Enter') {
       sendMessage();
     }
   };
 
+  const toggleRecording = () => {
+    setIsManuallyStopped(isRecording);
+    setIsRecording(!isRecording);
+    if (!isRecording) {
+      speechRecognitionRef.current.start();
+    } else {
+      speechRecognitionRef.current.stop();
+    }
+  };
+
   return (
     <div className="chat">
-    <div className="chat-header">
-      {isGptSpeaking && <div>GPT is speaking...</div>}
-    </div>
+<div className="chat-header">
+  <div className="assistant-label">
+    {/* <b>Assistant</b> */}
+  </div>
+</div>
+
       <div className="chat-messages" ref={chatMessagesRef}>
-        {messages.map(message => (
-          <div key={message.id} className={`message ${message.user.name === "rawhi" ? 'me' : 'other'}`}>
-            {message.text &&               <>
+      {messages.map(message => (
+          <div
+            key={message.id}
+            className={`message ${message.user.name === "rawhi" ? 'me' : 'other'}`}
+            style={{ display: "flex", marginBottom: "10px" }}
+          >
+            {message.user.name === "rawhi" ? (
+              <>
                 <div className="text" style={{ alignSelf: "center" }}>{message.text}</div>
                 <Avatar sx={{ bgcolor: message.user.color }} style={{ width: "40px", height: "40px", marginLeft: "10px" }}>{message.user.avatar}</Avatar>
-              </>}
-            {message.audioUrl &&               <>
+              </>
+            ) : (
+              <>
                 <Avatar sx={{ bgcolor: message.user.color }} style={{ width: "40px", height: "40px", marginRight: "10px" }}>{message.user.avatar}</Avatar>
-                <audio controls src={message.audioUrl}></audio>
-              </>}
+                <div className="text" style={{ alignSelf: "center" }}>{message.text}</div>
+              </>
+            )}
           </div>
         ))}
+          <div className="head">
+    <div className="eyebrow-container">
+      <div className="eyebrow">
+      </div>
+      <div className="eyebrow">
+      </div>
+    </div>
+    <div className="eyes-container">
+      <div className="eye">
+        <div className="pupil"></div>
+      </div>
+      <div className="eye">
+        <div className="pupil"></div>
+      </div>
+    </div>
+    <div className="nose"></div>
+    {isGptSpeaking ? <div className="mouth"></div> : <div className="nospeak-mouth"></div>}
+  </div>
       </div>
       <div className="chat-input">
         <input
@@ -177,8 +318,15 @@ const AudioChat = () => {
           onChange={e => setText(e.target.value)}
           onKeyDown={handleKeyDown}
         />
-        <Button disabled={!text} onClick={sendMessage} variant="contained" endIcon={<SendIcon />}>
+        <Button disabled={!text} onClick={() => sendMessage()} variant="contained" endIcon={<SendIcon />} style={{ minWidth: "90px" }}>
           Send
+        </Button>
+        <Button 
+          onMouseDown={startRecording} 
+          variant="contained" 
+          style={{ marginLeft: "2px", minWidth: "40px" }}
+        >
+          {isRecording ? <MicOffIcon  style={{ color: "red", marginLeft: "2px", minWidth: "40px" }} /> : <MicIcon onClick={play} />}
         </Button>
       </div>
     </div>
